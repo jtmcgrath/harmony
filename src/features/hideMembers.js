8499d88433f4178272c30a5100ff8a83fe6521e9
Joe McGrath <joe@raptori.dev>
2019-09-08 21:27:35 +0100

    make hideMembers optional

--- src/features/hideMembers.js
+++ src/features/hideMembers.js
@@ -1,19 +1,25 @@
 ;(function() {
-	const hideMembers = () => {
-		if (window.innerWidth < 900) {
-			if (document.querySelector('[class*="members"]')) {
-				document.querySelector('[aria-label="Member List"]').click()
+	chrome.storage.sync.get({ features: { members: true } }, ({ features }) => {
+		if (features.members) {
+			const hideMembers = () => {
+				if (window.innerWidth < 900) {
+					if (document.querySelector('[class*="members"]')) {
+						document
+							.querySelector('[aria-label="Member List"]')
+							.click()
+					}
+				}
 			}
+
+			window.addEventListener('load', () => {
+				setTimeout(hideMembers, 1000)
+			})
+
+			let debounceTimer
+			window.addEventListener('resize', () => {
+				clearTimeout(debounceTimer)
+				debounceTimer = setTimeout(hideMembers, 200)
+			})
 		}
-	}
-
-	window.addEventListener('load', () => {
-		setTimeout(hideMembers, 1000)
-	})
-
-	let debounceTimer
-	window.addEventListener('resize', () => {
-		clearTimeout(debounceTimer)
-		debounceTimer = setTimeout(hideMembers, 200)
 	})
 })()
